699dc785225931fb39fb1824c8854cc944a6db5b
Carl Lundin <carl.a.j.lundin@gmail.com>
2017-12-10 22:16:58 +0100

    Day 10 completed

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Day10
 {
@@ -8,59 +10,115 @@ namespace Day10
         // 38612 too high
         static int[] realInputLengths = new int[] {106, 118, 236, 1, 130, 0, 235, 254, 59, 205, 2, 87, 129, 25, 255, 118};
         static int[] testInputLengths = new int[] {2, 4, 1, 5};
-        static int[] testInputLengths2 = new int[] {5, 0, 1, 6};
-        static int[] testInputLengths3 = new int[] {2, 5};
 
         static int realInputLength = 256;
         static int testInputLength = 5;
-        static int testInputLength2 = 6;
-        static int testInputLength3 = 5;
 
-        static List<int> theList = new List<int>();
+        static string inputTextReal = "106,118,236,1,130,0,235,254,59,205,2,87,129,25,255,118";
+        
+        static string inputText1 = "";
+        static string inputText2 = "AoC 2017";
+        static string inputText3 = "1,2,3";
+        static string inputText4 = "1,2,4";
+
+        static byte[] extraLengths = new byte[] { 17, 31, 73, 47, 23};
 
         static void Main(string[] args)
         {
-            //Console.WriteLine("Test Result part 1: " + GetResult(testInputLengths, testInputLength));
-            //Console.WriteLine("Test Result part 1: " + GetResult(testInputLengths2, testInputLength2));
-            //Console.WriteLine("Test Result part 1: " + GetResult(testInputLengths3, testInputLength3));
-            Console.WriteLine("Result part 1: " + GetResult(realInputLengths, realInputLength));
-        }
-
-        static int GetResult(int[] lengths, int nrOfElements) 
-        {
-            for (var i = 0; i < nrOfElements; i++) 
-            {
-                theList.Add(i);
+            // This is the 255 long array... 1, 2, 3, 4 etc
+            var list = new int[realInputLength];
+            for (var i = 0; i < realInputLength; i++) {
+                list[i] = i;
             }
 
-            var currPosition = 0;
+            var result1 = ScrambleOneRound(list, realInputLengths, 0, 0);
+            Console.WriteLine("Result part 1: " + result1.Item1[0] * result1.Item1[1]);
+            
+            // #####################################################
+            // Part 2 
+
+            // This is the 255 long array... 1, 2, 3, 4 etc
+            list = new int[realInputLength];
+            for (var i = 0; i < realInputLength; i++) {
+                list[i] = i;
+            }
+
+            // Add extra lengths
+            var bytes = Encoding.ASCII.GetBytes(inputTextReal); // Change here for other input
+            List<byte> tempList = new List<byte>(bytes.Length + extraLengths.Length);
+            tempList.AddRange(bytes);
+            tempList.AddRange(extraLengths);
+            bytes = tempList.ToArray();
+            realInputLengths = GetIntArray(bytes);
+
+            // Scramble 64 times
+            var currPosition = 0; 
             var skipSize = 0;
+            for (var i = 0; i < 64; i++) {
+                var result2 = ScrambleOneRound(list, realInputLengths, currPosition, skipSize);
+                list = result2.Item1;
+                currPosition = result2.Item2;
+                skipSize = result2.Item3;
+            }
+
+            // XOR
+            var xorList = new int[16];
+            for (var i = 0; i < 16; i++) {
+                var xorValue = list[i*16];
+                for (var j = 1; j < 16; j++) {
+                    xorValue = xorValue ^ list[i*16 + j];
+                }
+
+                xorList[i] = xorValue;
+            }
+
+            // To string
+            string finalHash = BitConverter.ToString(GetByteArray(xorList)).Replace("-", string.Empty).ToLower();
+            Console.WriteLine("Result part 2: " + finalHash);
+        }
+        
+        static Tuple<int[], int, int> ScrambleOneRound(int[] list, int[] lengths, int currPosition, int skipSize) 
+        {
             for (int i = 0; i < lengths.Length; i++)
             {
                 var currLength = lengths[i];
-                if (currLength > 1 && currLength != theList.Count) {
-                    
-                    var theListCopy = new List<int>(theList);
+                if (currLength > 1 && currLength != list.Length) {                    
+                    int[] listCopy = new int[list.Length];
+                    Array.Copy(list, listCopy, list.Length);                    
 
                     var k = 0;
                     for (int j = currLength - 1; j >= 0; j--)
                     {
-                        var realPos = (currPosition + k) % theList.Count;
-                        var revPos = (currPosition + j) % theList.Count;
-                        theList[realPos] = theListCopy[revPos];
+                        var realPos = (currPosition + k) % list.Length;
+                        var revPos = (currPosition + j) % list.Length;
+                        list[realPos] = listCopy[revPos];
                         k++;
                     }
                 }
 
-                currPosition = (currPosition + currLength + skipSize) % theList.Count;
+                currPosition = (currPosition + currLength + skipSize) % list.Length;
                 skipSize++;
             }
 
-            var first = theList[0];
-            var second = theList[1];
-            var result = first * second;
+            return new Tuple<int[], int, int>(list, currPosition, skipSize);
+        }
 
-            return result;
+        static int[] GetIntArray(byte[] bytes) {
+            var ints = new int[bytes.Length];
+            for (var i = 0; i < bytes.Length; i++) {
+                ints[i] = (int)bytes[i];
+            }
+
+            return ints;
+        }
+
+        static byte[] GetByteArray(int[] ints) {
+            var bytes = new byte[ints.Length];
+            for (var i = 0; i < ints.Length; i++) {
+                bytes[i] = (byte)ints[i];
+            }
+
+            return bytes;
         }
     }
 }
