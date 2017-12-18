f0600ec0be0e177b4a1330b975db1949882236e1
Carl Lundin <carl.a.j.lundin@gmail.com>
2017-12-18 12:49:37 +0100

    Day 18 started

---
+++ Day17/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Result part 1: " + GetResult(376));
+            Console.WriteLine("Result part 2: " + GetResult2(376));
+        }
+
+        static int GetResult(int steps) {
+            var theList = new List<int>() { 0 };
+            var currPos = 0;
+
+            while (theList.Count < 2018) {
+                for (var i = 0; i < steps; i++) {
+                    currPos = (currPos + 1) % theList.Count;
+                }
+
+                currPos++;
+
+                var val = theList.Count;
+
+                theList.Insert(currPos, theList.Count);
+
+                if (currPos == 1) {
+                    Console.WriteLine(currPos + ": " + val);
+                }
+            } 
+
+            return theList[currPos + 1];
+        }
+        
+// 1, 23832
+// 1, 31082
+// 1, 48691
+// 1, 53719
+// 1, 60700
+// 1, 442596
+
+
+// 1: 37
+// 1: 239
+// 1: 361
+// 1: 392
+// 1: 823
+// 1: 950
+// 1: 1612
+        static int GetResult2(int steps) {
+            //var listSize = 442596;
+            
+            // var listSize = 392 + 1;
+            // //steps = steps;
+            // var currPos = 1;
+
+            // var listSize = 3 + 1;
+            // steps = 3;
+            // var currPos = 2;
+
+            var listSize = 23832 + 1;
+            var currPos = 1;
+            
+            var lastAddedOnPos1 = listSize - 1;
+
+            while (true) {
+                currPos += steps;
+
+                if (currPos == listSize - 1) {
+                    listSize++;
+                    //Console.WriteLine("hit last pos = " + listSize);
+                } else if (currPos >= listSize) {
+                    currPos = currPos - listSize; 
+                    if (currPos == 0) {
+                        lastAddedOnPos1 = listSize;
+                        Console.WriteLine("1 pos = " + listSize);
+                    }
+
+                    listSize++;
+                    currPos++;                    
+                } else {
+                    currPos++;
+                    listSize++;
+                    //Console.WriteLine("normal add");
+                }
+
+                //Console.WriteLine("round complete, " + currPos + ", " + listSize);
+
+                if (listSize > 50000) {
+                    return lastAddedOnPos1;
+                }
+            }
+        }
+    }
+}
---
+++ Day18/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day18
+{
+    class Program
+    {
+        static List<Tuple<string, string, string>> instructions = new List<Tuple<string, string, string>>();
+        static Dictionary<char, int> registers = new Dictionary<char, int>();
+        static int index = 0;
+        static int lastFreqSent = 0;
+
+        static void Main(string[] args)
+        {
+            SetupDataStructure("inputTest.txt");
+
+            Console.WriteLine("Result part 1: " + GetResult());
+        }
+
+        static int GetResult() 
+        {
+            char regName;
+            var value = 0;
+            int val;
+            while (true) {
+                try {
+                    var instruction = instructions[index];
+                    switch (instruction.Item1) {
+                        case "set":
+                            regName = instruction.Item2[0];
+                            value = int.TryParse(instruction.Item3, out val) ? val : registers[instruction.Item3[0]];
+                            registers[regName] = value;
+                            index++;
+                            break;
+                        case "add":
+                            regName = instruction.Item2[0];
+                            value = int.TryParse(instruction.Item3, out val) ? val : registers[instruction.Item3[0]];
+                            registers[regName] = registers[regName] + value;
+                            index++;
+                            break;
+                        case "mul":
+                            regName = instruction.Item2[0];
+                            value = int.TryParse(instruction.Item3, out val) ? val : registers[instruction.Item3[0]];
+                            registers[regName] = registers[regName] * value;
+                            index++;
+                            break;
+                        case "mod":
+                            regName = instruction.Item2[0];
+                            value = int.TryParse(instruction.Item3, out val) ? val : registers[instruction.Item3[0]];
+                            registers[regName] = registers[regName] % value;
+                            index++;
+                            break;
+                        case "snd":
+                            regName = instruction.Item2[0];
+                            lastFreqSent = registers[regName];
+                            index++;
+                            break;
+                        case "rcv":
+                            regName = instruction.Item2[0];
+                            registers[regName] = lastFreqSent;
+                            index++;
+                            break;
+                        case "jgz":
+                            regName = instruction.Item2[0];
+                            var regVal = registers[regName];
+                            if (regVal > 0) {
+                                var jumpVal = int.Parse(instruction.Item3);
+                                index += jumpVal;
+                            }
+
+                            break;
+                    }
+                }
+                catch (Exception e) {
+                    return lastFreqSent;
+                }
+            }
+        }
+
+        static void SetupDataStructure(string file) {
+            var inputs = System.IO.File.ReadAllLines(file);
+            for (var j = 0; j < inputs.Length; j++) {
+                var instrs = inputs[j].Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+                
+                var regName = instrs[0];                            
+                string var1 = instrs[1];
+                string var2 = instrs.Length == 3 ? instrs[2] : null;
+
+                instructions.Add(new Tuple<string, string, string>(regName, var1, var2));
+            }
+
+            registers.Add('a', 0);
+            registers.Add('b', 0);
+            registers.Add('f', 0);
+            registers.Add('i', 0);
+            registers.Add('p', 0);
+        }
+    }
+}
