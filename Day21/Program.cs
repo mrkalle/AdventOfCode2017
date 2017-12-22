f5578e5635af5710df07f1259735223291f6442d
Carl Lundin <carl.a.j.lundin@gmail.com>
2017-12-22 11:35:53 +0100

    Day 21 started. Day 22 completed.

---
+++ Day21/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day21
+{
+    class Program
+    {
+        static List<Dictionary<string, string>> rules = new List<Dictionary<string, string>>();
+        static string grid = ".#./..#/###";
+        
+        static void Main(string[] args)
+        {
+            SetupDataStructure("inputTest.txt");
+            //SetupDataStructure("input.txt");
+            Console.WriteLine("Result part 1: " + GetNrOfOnPixels());
+        }
+
+        static int GetNrOfOnPixels() {
+            for (var i = 0; i < 5; i++) {
+                var gridSize = GetGridSize(grid);
+                var size = gridSize % 2 == 0 ? 2 : 3;
+
+                var subSquares = GetSubSquares(grid, size); // Divide into subs
+
+                var largerSubSquares = new List<string>();
+                foreach (var subSquare in subSquares) {
+                    largerSubSquares.Add(GetLargerSquare(subSquare, size)); // Make larger
+                }
+
+                if (largerSubSquares.Count > 1) {
+                    SetCommonSquare(largerSubSquares, size); // Unite
+                } else {
+                    grid = largerSubSquares[0];
+                }
+            }
+
+            var nrOfOnPixels = grid.Replace(".", "").Length;
+            return nrOfOnPixels;
+        }
+
+        static void SetCommonSquare(List<string> subSquares, int size) {
+            var nrOfSubSquares = subSquares.Count;
+            var gridSize = GetGridSize(subSquares[0]);
+            var nrOfRowsInCommonSquare = (nrOfSubSquares/size) * gridSize;
+            
+            for (var i = 0; i < subSquares.Count; i++) {
+                subSquares[i] = subSquares[i].Replace("/", "");    
+            }     
+
+            var newGrid = "";
+
+            if (nrOfSubSquares > 1) {
+                var newGridList = new List<string>();
+                for (var i = 0; i < nrOfRowsInCommonSquare; i++) {
+                    newGridList.Add("");
+                }
+
+// FUUUUUUUCK!!!
+
+                for (var i = 0; i < gridSize; i++) {
+                    for (var j = 0; j < gridSize; j++) {
+                        var subSquareIndex = (int)(Math.Floor((double)(i/gridSize))*gridSize + Math.Floor((double)(j/gridSize)));
+
+                        newGridList[subSquareIndex] = newGridList[subSquareIndex] + subSquares[subSquareIndex].Substring(j*gridSize, gridSize);
+                    }  
+                }
+                
+                newGrid = string.Join("", newGridList);
+            }
+
+            // Lägg in radbryt å...
+
+            for (var i = 0; i < nrOfSubSquares; i++) {
+                for (var j = size - 2; j >= 0; j--) {
+                    subSquares[i] = subSquares[i].Insert(size*j + size, "/");
+                }
+            }
+
+            grid = subSquares[0];
+        }
+
+        static List<string> GetSubSquares(string oldSquare, int size) {
+            var gridSize = GetGridSize(oldSquare);
+            var nrOfSubSquares = gridSize / size;
+            nrOfSubSquares = nrOfSubSquares == 1 ? nrOfSubSquares : nrOfSubSquares*2;
+            oldSquare = oldSquare.Replace("/", "");
+            var subSquares = new List<string>();
+
+            for (var i = 0; i < nrOfSubSquares; i++) {
+                subSquares.Add("");
+            }
+
+            for (var i = 0; i < gridSize; i++) {
+                for (var j = 0; j < gridSize; j++) {
+                    var subSquareIndex = (int)(Math.Floor((double)(i/size))*size + Math.Floor((double)(j/size)));
+                    subSquares[subSquareIndex] = subSquares[subSquareIndex] + oldSquare[i*gridSize + j];
+                }  
+            }
+
+            for (var i = 0; i < nrOfSubSquares; i++) {
+                for (var j = size - 2; j >= 0; j--) {
+                    subSquares[i] = subSquares[i].Insert(size*j + size, "/");
+                }
+            }
+
+            return subSquares;
+        } 
+
+        static string GetLargerSquare(string oldSquare, int size) {
+            var newSquare = oldSquare;
+
+            for (var i = 0; i < 4; i++) {
+                newSquare = GetRotatedSquare(newSquare, size);
+                if (rules[size].ContainsKey(newSquare)) {
+                    return rules[size][newSquare];
+                }
+
+                newSquare = GetFlippedSquare(newSquare, size);
+                if (rules[size].ContainsKey(newSquare)) {
+                    return rules[size][newSquare];
+                }
+
+                newSquare = GetFlippedSquare(newSquare, size); // Flip back... just because..
+            }
+
+            return null;
+        }        
+
+        static string GetRotatedSquare(string oldSquare, int size) {
+            oldSquare = oldSquare.Replace("/", "");
+            var newSquare = "";
+
+            for (var i = 0; i < size; i++) {
+                for (var j = size - 1; j >= 0; j--) {
+                    newSquare += oldSquare[j*size + i];
+                }
+
+                newSquare += "/";
+            }
+
+            return newSquare.Substring(0, newSquare.Length - 1);
+        }
+
+        static string GetFlippedSquare(string oldSquare, int size) {
+            oldSquare = oldSquare.Replace("/", "");
+            var newSquare = "";
+
+            for (var i = 0; i < size; i++) {
+                for (var j = size - 1; j >= 0; j--) {
+                    newSquare += oldSquare[i*size + j];
+                }
+
+                newSquare += "/";
+            }
+
+            return newSquare.Substring(0, newSquare.Length - 1);
+        }
+        
+        static int GetGridSize(string inputGrid) {
+            return inputGrid.IndexOf("/");
+        }
+    
+        static void SetupDataStructure(string file) {
+            var inputs = System.IO.File.ReadAllLines(file);
+            rules.Add(new Dictionary<string, string>());
+            rules.Add(new Dictionary<string, string>());
+            rules.Add(new Dictionary<string, string>());
+            rules.Add(new Dictionary<string, string>());
+            for (var i = 0; i < inputs.Length; i++) {
+                var rule = inputs[i];   
+                var parts = rule.Split(new string[] { " => " }, StringSplitOptions.RemoveEmptyEntries);                
+                if (GetGridSize(rule) == 2) {  
+                    rules[2].Add(parts[0], parts[1]);
+                } else {         
+                    rules[3].Add(parts[0], parts[1]);
+                }
+            }
+        }
+    }
+}
---
+++ Day22/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day22
+{
+    class Program
+    {
+        static int maxSize = 240;
+
+        static int currX = 0;
+        static int currY = 0;
+        static int currDir = 0;
+
+        static int currNrOfInfectingBursts = 0;
+
+        static Dictionary<Tuple<int,int>, char> originalGrid = new Dictionary<Tuple<int,int>, char>();
+        static Dictionary<Tuple<int,int>, char> grid = new Dictionary<Tuple<int,int>, char>();
+
+        static void Main(string[] args)
+        {
+            //SetupDataStructure("inputTest.txt");
+            SetupDataStructure("input.txt");
+            var res = GetNrOfInfectedNodes();
+            Console.WriteLine("Result part 1: " + res);
+
+            //SetupDataStructure("inputTest.txt");
+            SetupDataStructure("input.txt");
+            res = GetNrOfInfectedNodes2();
+            Console.WriteLine("Result part 2: " + res);
+        }
+
+        static int GetNrOfInfectedNodes() 
+        {
+            for (var i = 0; i < 10000; i++) {                               
+                Turn();
+                InfectOrCleanNode();
+                MoveForward();
+                //Console.WriteLine("{0},{1} dir {2}", currX, currY, currDir);
+                //Console.WriteLine("i: {0}", i);
+                //PrintGrid(grid);
+            }
+
+            return currNrOfInfectingBursts;       
+        }
+
+        static int GetNrOfInfectedNodes2() {
+
+            //Console.WriteLine("{0},{1} dir {2}", currX, currY, currDir);
+            //Console.WriteLine("i: {0}", "start");
+            //PrintGrid(grid);
+
+            for (var i = 0; i < 10000000; i++) {                               
+                Turn2();
+                ChangeState();
+                MoveForward();
+                //Console.WriteLine("{0},{1} dir {2}", currX, currY, currDir);
+                //Console.WriteLine("i: {0}", i);
+                //PrintGrid(grid);
+            }
+        
+            //PrintGrid(grid);
+            
+            return currNrOfInfectingBursts;       
+        }
+
+        static int GetResult() {
+            var result = 0;
+
+            for (var i = -maxSize; i < maxSize; i++) {             
+                for (var j = -maxSize; j < maxSize; j++) {
+                    var currPos = new Tuple<int,int>(i,j);
+                    if (originalGrid[currPos] == '.' && grid[currPos] == '#') {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        static void Turn() {
+            var isInfected = grid[new Tuple<int,int>(currX, currY)] == '#';
+
+            if (isInfected) {
+                currDir++;
+
+                if (currDir == 4) {
+                    currDir = 0;
+                }
+            } else {
+                currDir--;
+
+                if (currDir == -1) {
+                    currDir = 3;
+                }
+            }
+        }
+
+        static void Turn2() {
+            var startState = grid[new Tuple<int,int>(currX, currY)];
+
+            switch (startState) {
+                case '.':
+                    currDir--;
+
+                    if (currDir == -1)
+                    {
+                        currDir = 3;
+                    }
+
+                    return;
+                case 'W':
+                    return;
+                case '#':
+                    currDir++;
+
+                    if (currDir == 4) {
+                        currDir = 0;
+                    }
+
+                    return;
+                case 'F':
+                    switch (currDir) {
+                        case 0:
+                            currDir = 2;
+                            return;
+                        case 1:
+                            currDir = 3;
+                            return;
+                        case 2:
+                            currDir = 0;
+                            return;
+                        case 3:
+                            currDir = 1;
+                            return;
+                    }
+
+                    throw new Exception("WTF weird dir on state..");
+            }
+            
+            throw new Exception("WTF Faulty state: " + startState);
+        }
+
+        static void InfectOrCleanNode() {
+            if (grid[new Tuple<int,int>(currX, currY)] == '.') {
+                grid[new Tuple<int,int>(currX, currY)] = '#';
+                currNrOfInfectingBursts++;
+            } else {                
+                grid[new Tuple<int,int>(currX, currY)] = '.';
+            }
+        }
+
+        static void ChangeState() {            
+            var startState = grid[new Tuple<int,int>(currX, currY)];
+            switch (startState) {
+                case '.':
+                    grid[new Tuple<int,int>(currX, currY)] = 'W';
+                    return;
+                case 'W':
+                    grid[new Tuple<int,int>(currX, currY)] = '#';
+                    currNrOfInfectingBursts++;
+                    return;
+                case '#':
+                    grid[new Tuple<int,int>(currX, currY)] = 'F';
+                    return;
+                case 'F':
+                    grid[new Tuple<int,int>(currX, currY)] = '.';
+                    return;
+            }
+            
+            throw new Exception("WTF Faulty state: " + startState);
+        }
+
+        static void MoveForward() {
+            switch (currDir) {
+                case 0:
+                    currX--;
+                    return;
+                case 1: 
+                    currY++;
+                    return;
+                case 2:
+                    currX++;
+                    return;
+                case 3: 
+                    currY--;
+                    return;
+                default:
+                    throw new Exception("WTF DIR " + currDir);
+            }
+        }
+        
+        static void SetupDataStructure(string file) {
+            currX = 0;
+            currY = 0;
+            currDir = 0;
+            currNrOfInfectingBursts = 0;
+
+            originalGrid = new Dictionary<Tuple<int,int>, char>();
+            grid = new Dictionary<Tuple<int,int>, char>();
+
+            var inputs = System.IO.File.ReadAllLines(file);
+            var xOffset = inputs.Length/2; 
+            var yOffset = xOffset;
+
+            for (var i = -maxSize; i < maxSize; i++) {
+                for (var j = -maxSize; j < maxSize; j++) {
+                    grid.Add(new Tuple<int,int>(i, j), '.');
+                    originalGrid.Add(new Tuple<int,int>(i, j), '.');
+                }
+            }
+
+            for (var i = 0; i < inputs.Length; i++) {
+                var row = inputs[i];                
+                for (var j = 0; j < row.Length; j++) {
+                    grid[new Tuple<int,int>(i-xOffset, j-yOffset)] = row[j];
+                    originalGrid[new Tuple<int,int>(i-xOffset, j-yOffset)] = row[j];
+                }
+            }
+        }
+
+        static void PrintGrid(Dictionary<Tuple<int,int>, char> printGrid) {
+            for (var i = -maxSize; i < maxSize; i++) {             
+                for (var j = -maxSize; j < maxSize; j++) {
+                    if (i == currX && j == currY) {
+                        Console.Write(printGrid[new Tuple<int,int>(i, j)] + "<");
+                    } else {                        
+                        Console.Write(printGrid[new Tuple<int,int>(i, j)] + " ");
+                    }
+                }
+
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("------------------------------------------");
+        }
+    }
+}
+
+
+// . . . . . . . . .
+// . . . . . . . . .
+// . . . . . . . . .
+// . . . . . # . . .
+// . . . #[.]. . . .
+// . . . . . . . . .
+// . . . . . . . . .
+// . . . . . . . . .
+
+
+// . . . . . # # . .
+// . . . . # . . # .
+// . . . # . . . . #
+// . . # . #[.]. . #
+// . . # . # . . # .
+// . . . . . # # . .
+// . . . . . . . . .
+// . . . . . . . . .
