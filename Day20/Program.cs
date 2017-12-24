9796a4f1fd18961f1e709288e763a553b86401b6
Carl Lundin <carl.a.j.lundin@gmail.com>
2017-12-24 11:48:38 +0100

    Day 20 part 2 longer..

--- Day20/Program.cs
+++ Day20/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 namespace Day20
@@ -9,12 +10,80 @@ namespace Day20
         static int[][] p;
         static int[][] v;
         static int[][] a;
+
+        static List<Tuple<int,int,int>> pList = new List<Tuple<int,int,int>>();
+        static List<Tuple<int,int,int>> vList = new List<Tuple<int,int,int>>();
+        static List<Tuple<int,int,int>> aList = new List<Tuple<int,int,int>>();
+        static List<int> collisions = new List<int>();
         
         static void Main(string[] args)
         {
             //SetupDataStructure("inputTest.txt");
             SetupDataStructure("input.txt");
-            Console.WriteLine("Result part 1: " + GetResult());
+            //Console.WriteLine("Result part 1: " + GetResult());
+            
+            Console.WriteLine("Result part 2: " + GetResult2());
+        }
+
+        static BigInteger GetResult2() 
+        {
+            MarkParticlesForCollisions();
+            RemoveCollidingParticles();
+
+            for (var i = 0; i < 100000; i++) 
+            {
+                SetNewPositions();
+                MarkParticlesForCollisions();
+                RemoveCollidingParticles();
+            }
+
+            return pList.Count;
+        }
+
+        static void RemoveCollidingParticles() {
+            for (var i = collisions.Count - 1; i >= 0; i--) {
+                pList.RemoveAt(i);
+                vList.RemoveAt(i);
+                aList.RemoveAt(i);
+            }
+        }
+
+        static void MarkParticlesForCollisions() {
+            collisions = new List<int>();
+            for (var i = 0; i < pList.Count - 1; i++) {
+                for (var j = i + 1; j < pList.Count; j++) {
+                    if (pList[i].Item1 == pList[j].Item1 &&
+                        pList[i].Item2 == pList[j].Item2 &&
+                        pList[i].Item3 == pList[j].Item3) {
+
+                        collisions.Add(i);
+                        collisions.Add(j);
+                    }
+                }
+            }
+
+            if (collisions.Count > 0) {
+                collisions = collisions.Distinct().ToList();
+            }
+        }
+
+        static void SetNewPositions() {
+            for (var i = 0; i < pList.Count; i++) {
+                var vel = new int[3];
+                var pos = new int[3];
+
+                var accOld = new int[] {aList[i].Item1, aList[i].Item2, aList[i].Item3};
+                var velOld = new int[] {vList[i].Item1, vList[i].Item2, vList[i].Item3};
+                var posOld = new int[] {pList[i].Item1, pList[i].Item2, pList[i].Item3};
+
+                for (var j = 0; j < 3; j++) {
+                    vel[j] = velOld[j] + accOld[j];   
+                    pos[j] = posOld[j] + vel[j];    
+                }
+
+                vList[i] = new Tuple<int,int,int>(vel[0], vel[1], vel[2]);
+                pList[i] = new Tuple<int,int,int>(pos[0], pos[1], pos[2]);
+            }
         }
 
         static BigInteger GetResult() 
@@ -38,8 +107,6 @@ namespace Day20
                 }
             }
 
-            // Hitta nod med lägst absolut belopp och returnera det indexet
-
             return lowestIndex;
         }
         
@@ -54,10 +121,13 @@ namespace Day20
                     var o = GetData(dataPairs[j]);   
                     if (j == 0) {
                         p[i] = new int[] {o.Item1, o.Item2, o.Item3};
+                        pList.Add(new Tuple<int,int,int>(o.Item1, o.Item2, o.Item3));
                     } else if (j == 1) {
                         v[i] = new int[] {o.Item1, o.Item2, o.Item3};
+                        vList.Add(new Tuple<int,int,int>(o.Item1, o.Item2, o.Item3));
                     } else if (j == 2) {
                         a[i] = new int[] {o.Item1, o.Item2, o.Item3};
+                        aList.Add(new Tuple<int,int,int>(o.Item1, o.Item2, o.Item3));
                     } else {
                         throw new Exception("WTF error");
                     }          
